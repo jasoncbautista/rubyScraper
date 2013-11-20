a75f881f3b7d64ee5261461415e5dd9cf1a66148
Jason <jason.bautista@sqor.com>
2013-11-20 11:06:06 -0800

    scraper

--- scrape.rb
+++ scrape.rb
@@ -4,66 +4,76 @@ require 'open-uri'
 # Get a Nokogiri::HTML::Document for the page we’re interested in...
 
 
-myFile = "./fightersJSON.txt"
+myFile = "./jsonFighters.txt"
  File.open(myFile, 'w') { |file|
      file.write(" {  fighters: [ \n") 
  
      fighterCount = 0
 
-    doc = Nokogiri::HTML(open('http://www.ufc.com/fighter/Weight_Class/Bantamweight?offset=20&max=20&sort=lastName&order=asc'))
-
-    # Do funky things with it using Nokogiri::XML::Node methods...
-
-    ####
-    # Search for all the fighters in the page
-    doc.css('tr.fighter').each do |fighterCard|
-        begin
-        # Find fighter names:
-            names = fighterCard.css('a.fighter-name')
-
-            name = names.first.content
-
-            cells = fighterCard.css(".cell-inner")
-
-            # Get other MISC info:
-            
-            # [0] = Name 
-            # [1] = Record 
-            # [2] = Height
-            # [3] = Weight
-
-            recordCell = cells[1]
-            heightCell = cells[2]
-            weightCell = cells[3]
+    ii = 0
+    max = 2
 
 
-            # Extract the weight
-            weightLB = weightCell.css(".main-txt").first.content
+    while ii < max do
+        offset = 20*ii
+        ii+=1
+        doc = Nokogiri::HTML(open('http://www.ufc.com/fighter/Weight_Class?offset=' + offset.to_s + '&max=20&sort=lastName&order=asc'))
 
-            # Extract the height:
-            heightCM = weightCell.css(".sub-txt").first.content
+        # Do funky things with it using Nokogiri::XML::Node methods...
 
-            
-            # Prepare js
-            jsonName = " 'name' :  \"" + name.strip + "\""
-            jsonWeight = ", 'weight': " + weightLB.strip
-            jsonHeight= ", 'height': " + heightCM.strip
-            str = jsonName + jsonWeight + jsonHeight
+        ####
+        # Search for all the fighters in the page
+        doc.css('tr.fighter').each do |fighterCard|
+            begin
+            # Find fighter names:
+                names = fighterCard.css('a.fighter-name')
 
-            # Write out to file (as json)
-            if fighterCount != 0 
-            file.write(" ,  ") 
+                name = names.first.content
+
+                cells = fighterCard.css(".cell-inner")
+
+                # Get other MISC info:
+                
+                # [0] = Name 
+                # [1] = Record 
+                # [2] = Height
+                # [3] = Weight
+
+                recordCell = cells[1]
+                heightCell = cells[2]
+                weightCell = cells[3]
+
+
+                # Extract the weight
+                weightLB = weightCell.css(".main-txt").first.content
+
+                # Extract the height:
+                heightCM = weightCell.css(".sub-txt").first.content
+
+                
+                # Prepare js
+                jsonName = " 'name' :  \"" + name.strip + "\""
+                jsonWeight = ", 'weight': \"" + weightLB.strip + "\""
+                jsonHeight= ", 'height': \"" + heightCM.strip + "\""
+                str = jsonName + jsonWeight + jsonHeight
+
+                # Write out to file (as json)
+                if fighterCount != 0 
+                    file.write(" ,  ") 
+                end
+                puts name
+
+                file.write(" {  ") 
+                file.write(str)
+                file.write(" } \n ") 
+                fighterCount+=1
+            rescue
+                puts "Bad node"
             end
-            file.write(" {  ") 
-            file.write(str)
-            file.write(" } \n ") 
-            fighterCount+=1
-        rescue
-            puts "Bad node"
+
         end
 
     end
-
 
     file.write("   ] }\n") 
  }
