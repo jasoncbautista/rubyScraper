fd8a021c4564e1a020008cac6ee429923d010270
Jason <jason.bautista@sqor.com>
2013-11-21 14:16:00 -0800

    output to stdout version

--- scrape.rb
+++ scrape.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-# HERE be dragons: 
+# HERE be dragons: gotta learn to use ruby functions.
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 # Get a Nokogiri::HTML::Document for the page we’re interested in...
@@ -19,22 +19,20 @@ myFile = "./jsonFighters.txt"
     max = 2
 
     dataString = ""
+    # Loop over multiple pages:
     while ii < max do
         offset = 20*ii
         ii+=1
+        # Prepare the URL:
         doc = Nokogiri::HTML(open('http://www.ufc.com/fighter/Weight_Class?offset=' + offset.to_s + '&max=20&sort=lastName&order=asc'))
-
-        # Do funky things with it using Nokogiri::XML::Node methods...
 
         ####
         # Search for all the fighters in the page
         doc.css('tr.fighter').each do |fighterCard|
             begin
-            # Find fighter names:
+                # Find fighter names:
                 names = fighterCard.css('a.fighter-name')
-
                 name = names.first.content
-
                 cells = fighterCard.css(".cell-inner")
 
                 # Get other MISC info:
@@ -43,28 +41,24 @@ myFile = "./jsonFighters.txt"
                 # [1] = Record 
                 # [2] = Height
                 # [3] = Weight
-
                 recordCell = cells[1]
                 heightCell = cells[2]
                 weightCell = cells[3]
 
-
                 # Extract the weight
-                weightLB = weightCell.css(".main-txt").first.content
+                weightLB = weightCell.css(".main-txt").first.content.sub("lbs", "")
 
                 # Extract the height:
-                heightCM = heightCell.css(".sub-txt").first.content
+                heightCM = heightCell.css(".sub-txt").first.content.sub("cm", "").sub("(", "").sub(")", "")
 
-                
                 # Prepare json
                 firstName= name.strip.split(" ")[0]
                 lastName= name.strip.split(" ")[1]
                 jsonFirstName = " 'first_name' :  \"" + firstName+ "\""
-                jsonLastName = " 'last_name' :  \"" + lastName + "\""
+                jsonLastName = ",  'last_name' :  \"" + lastName + "\""
                 jsonWeight = ", 'weight': \"" + weightLB.strip + "\""
                 jsonHeight= ", 'height': \"" + heightCM.strip + "\""
                 str = jsonFirstName + jsonLastName + jsonWeight + jsonHeight
-
 
                 # Write out to file (as json)
                 if fighterCount != 0 
@@ -75,22 +69,22 @@ myFile = "./jsonFighters.txt"
                 # Saving our data t be put into 
                 dataString += "{" + str + "}"
 
-
+                # Write ut to file
                 file.write(" {  ") 
                 file.write(str)
                 file.write(" } \n ") 
+
                 fighterCount+=1
             rescue
                 #puts "Bad node"
             end
-            
         end
 
-        # Need to make sure we dont abuse :
+        # Need to make sure we dont abuse the server:
         sleep(3) # Sleep for a few  seconds
     end
 
-    toStdOutString = "{  fighters: [ " + dataString +  "] }"
+    toStdOutString = "{  'fighters': [ " + dataString +  "] }"
     puts toStdOutString
 
     file.write("   ] }\n") 
