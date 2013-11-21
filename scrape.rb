16b4e8306336b87e9c82624cfc0075641f828e2d
Jason <jason.bautista@sqor.com>
2013-11-21 14:20:21 -0800

    Readme updated

--- scrape.rb
+++ scrape.rb
@@ -1,91 +1,75 @@
 require 'nokogiri'
 require 'open-uri'
 
-
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # HERE be dragons: gotta learn to use ruby functions.
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
-# Get a Nokogiri::HTML::Document for the page we’re interested in...
+# Define some helper variables:
+fighterCount = 0
+ii = 0
+max = 2
+dataString = ""
 
+# Loop over multiple pages:
+while ii < max do
+    offset = 20*ii
+    ii+=1
+    # Prepare the URL:
+    doc = Nokogiri::HTML(open('http://www.ufc.com/fighter/Weight_Class?offset=' + offset.to_s + '&max=20&sort=lastName&order=asc'))
 
-myFile = "./jsonFighters.txt"
- File.open(myFile, 'w') { |file|
-     file.write(" {  fighters: [ \n") 
- 
-     fighterCount = 0
+    ####
+    # Search for all the fighters in the page
+    doc.css('tr.fighter').each do |fighterCard|
+        begin
+            # Find fighter names:
+            names = fighterCard.css('a.fighter-name')
+            name = names.first.content
+            cells = fighterCard.css(".cell-inner")
 
-    ii = 0
-    max = 2
+            # Get other MISC info:
+            
+            # [0] = Name 
+            # [1] = Record 
+            # [2] = Height
+            # [3] = Weight
+            recordCell = cells[1]
+            heightCell = cells[2]
+            weightCell = cells[3]
 
-    dataString = ""
-    # Loop over multiple pages:
-    while ii < max do
-        offset = 20*ii
-        ii+=1
-        # Prepare the URL:
-        doc = Nokogiri::HTML(open('http://www.ufc.com/fighter/Weight_Class?offset=' + offset.to_s + '&max=20&sort=lastName&order=asc'))
+            # Extract the weight
+            weightLB = weightCell.css(".main-txt").first.content.sub("lbs", "")
 
-        ####
-        # Search for all the fighters in the page
-        doc.css('tr.fighter').each do |fighterCard|
-            begin
-                # Find fighter names:
-                names = fighterCard.css('a.fighter-name')
-                name = names.first.content
-                cells = fighterCard.css(".cell-inner")
+            # Extract the height:
+            heightCM = heightCell.css(".sub-txt").first.content.sub("cm", "").sub("(", "").sub(")", "")
 
-                # Get other MISC info:
-                
-                # [0] = Name 
-                # [1] = Record 
-                # [2] = Height
-                # [3] = Weight
-                recordCell = cells[1]
-                heightCell = cells[2]
-                weightCell = cells[3]
+            # Prepare json
+            firstName= name.strip.split(" ")[0]
+            lastName= name.strip.split(" ")[1]
+            jsonFirstName = " 'first_name' :  \"" + firstName+ "\""
+            jsonLastName = ",  'last_name' :  \"" + lastName + "\""
+            jsonWeight = ", 'weight': \"" + weightLB.strip + "\""
+            jsonHeight= ", 'height': \"" + heightCM.strip + "\""
+            str = jsonFirstName + jsonLastName + jsonWeight + jsonHeight
 
-                # Extract the weight
-                weightLB = weightCell.css(".main-txt").first.content.sub("lbs", "")
-
-                # Extract the height:
-                heightCM = heightCell.css(".sub-txt").first.content.sub("cm", "").sub("(", "").sub(")", "")
-
-                # Prepare json
-                firstName= name.strip.split(" ")[0]
-                lastName= name.strip.split(" ")[1]
-                jsonFirstName = " 'first_name' :  \"" + firstName+ "\""
-                jsonLastName = ",  'last_name' :  \"" + lastName + "\""
-                jsonWeight = ", 'weight': \"" + weightLB.strip + "\""
-                jsonHeight= ", 'height': \"" + heightCM.strip + "\""
-                str = jsonFirstName + jsonLastName + jsonWeight + jsonHeight
-
-                # Write out to file (as json)
-                if fighterCount != 0 
-                    file.write(" ,  ") 
-                    dataString+= ", "
-                end
-                
-                # Saving our data t be put into 
-                dataString += "{" + str + "}"
-
-                # Write ut to file
-                file.write(" {  ") 
-                file.write(str)
-                file.write(" } \n ") 
-
-                fighterCount+=1
-            rescue
-                #puts "Bad node"
+            # Write out to file (as json)
+            if fighterCount != 0 
+                dataString+= ", "
             end
-        end
+            
+            # Saving our data t be put into 
+            dataString += "{" + str + "}"
 
-        # Need to make sure we dont abuse the server:
-        sleep(3) # Sleep for a few  seconds
+            fighterCount+=1
+        rescue
+            #puts "Bad node"
+        end
     end
 
-    toStdOutString = "{  'fighters': [ " + dataString +  "] }"
-    puts toStdOutString
+    # Need to make sure we dont abuse the server:
+    sleep(3) # Sleep for a few  seconds
+end
 
-    file.write("   ] }\n") 
- }
+toStdOutString = "{  'fighters': [ " + dataString +  "] }"
+puts toStdOutString
+
